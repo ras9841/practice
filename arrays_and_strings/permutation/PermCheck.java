3248b1002a85cff6c43a81201d8cd842c4a87a91
Roland Sanford <ras9841@rit.edu>
2016-06-09 20:20:23 -0400

    Added test cases and case insensitivity.

--- arrays_and_strings/permutation/PermCheck.java
+++ arrays_and_strings/permutation/PermCheck.java
@@ -22,14 +22,14 @@ public class PermCheck
         if (w1.length() != w2.length()) return false;
         
         // Add the first word to the map
-        for (Character c : w1.toCharArray())
+        for (Character c : w1.toLowerCase().toCharArray())
         {
             if (dict.containsKey(c)) { dict.replace(c, dict.get(c)+1); }
             else { dict.put(c, (Integer)1); }
         }
        
         // Check for same letters and letter frequency
-        for (Character c : w2.toCharArray())
+        for (Character c : w2.toLowerCase().toCharArray())
         {
             if (!dict.containsKey(c)) { return false; }
             else if (dict.get(c).equals((Integer) 0)) { return false; }
--- arrays_and_strings/permutation/PermutationTest.java
+++ arrays_and_strings/permutation/PermutationTest.java
@@ -9,8 +9,23 @@ public class PermutationTest
     
     public static void main(String[] args)
     {
-        //TC1:  valid permutation
-        assert test.test("slab", "labs"): "\nFaled test case 1";
+        //TC1: empty strings 
+        assert test.test("", ""): "\nFaled test case 1";
+        
+        //TC2: valid permutation
+        assert test.test("slab", "labs"): "\nFaled test case 2";
+
+        //TC3: invalid permutation
+        assert !test.test("slab", "rock"): "\nFaled test case 3";
+
+        //TC4: case test
+        assert test.test("slab", "SLAB"): "\nFaled test case 4";
+        
+        //TC5: length test
+        assert !test.test("slab", "slabs"): "\nFaled test case 5";
+
+        //TC6: repeated letters
+        assert !test.test("slab", "lass"): "\nFaled test case 6";
 
         System.out.println("All tests passed.");
     }
