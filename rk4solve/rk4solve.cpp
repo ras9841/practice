fa04c4e8441cd089d7d4d592c9a350462fb601d0
Roland Sanford <ras9841@rit.edu>
2016-08-23 14:26:00 -0400

    Passed linear test.

--- rk4solve/rk4solve.cpp
+++ rk4solve/rk4solve.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-
-double rk4solve(double y0, double t, double h, double (*deriv)(double, double))
+double rk4solve(double yn, double t, double h, double (*deriv)(double, double))
 {
+    double k1, k2, k3, k4; 
     
-    
-    return 0;
+    k1 = deriv(yn, t); 
+    k2 = deriv(yn+(h/2)*k1, t+h/2); 
+    k3 = deriv(yn+(h/2)*k2, t+h/2); 
+    k4 = deriv(yn+h*k3, t+h); 
+
+    return yn + (h/6)*(k1 + 2*k2 + 2*k3 + k4);
 }
--- rk4solve/testRK.cpp
+++ rk4solve/testRK.cpp
@@ -9,20 +9,31 @@ double dydx(double y, double t)
 int main()
 {
     double t_0 = 0, t_f = 10, h = 1;
-    double y[(int)((t_f-t_0)/h)+1];
+    int num_steps = (int)((t_f-t_0)/h)+1;
+    double y[num_steps], time[num_steps];
 
     // Initial condition
     double y0 = 0;
     y[0] = y0;
+    time[0] = t_0;
 
     std::cout << "Testing \n";
-    double t;
+    double t = t_0;
     int i = 1;
     while (t<t_f+h)
     {
-        y0 = y[(i++)-1];
-        rk4solve(y0, t, h, &dydx);
+        y0 = y[i-1];
+        y[i] = rk4solve(y0, t, h, &dydx);
+        
         t += h;
+        time[i++] = t; 
     }
+
+    for (int i=0; i<num_steps; i++)
+    {
+        std::cout << "t=" << time[i] << "\t";
+        std::cout << "y=" << y[i] << "\n";
+    }
+
     return 0;
 }
